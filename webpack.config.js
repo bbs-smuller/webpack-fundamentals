548c649c7db0c92260e947b44f4e8de12883b01e
Sébastien Muller <smuller@big-boss-studio.com>
2017-07-24 17:50:24 +0200

    Use webpack configuration objects

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const Path = require('path');
 
-module.exports = {
-	entry: './src',
-	output: {
-		filename: '[chunkhash].bundle.js',
-		path: Path.join(__dirname, 'dist')
-	}
+module.exports = (env) => {
+	console.log('Webpack ENV', env);
+
+	return {
+		entry: './src/',
+		output: {
+			filename: '[chunkhash].bundle.js',
+			path: Path.join(__dirname, 'dist')
+		}
+	};
 };
